c42dc60912f56f4a77bca93c4bea7aacf7118e30
Student <example@example.com>
2018-03-16 12:19:02 -0700

    send a note to another activity from main

--- app/src/main/java/pinkjacket/swordslist/MainActivity.java
+++ app/src/main/java/pinkjacket/swordslist/MainActivity.java
@@ -1,13 +1,41 @@
 package pinkjacket.swordslist;
 
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.titleTextView) TextView mTitleTextView;
+    @BindView(R.id.noteEditText) EditText mNoteEditText;
+    @BindView(R.id.noteButton) Button mNoteButton;
+    @BindView(R.id.listButton) Button mListButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface aquiline = Typeface.createFromAsset(getAssets(), "fonts/aquiline.ttf");
+        mTitleTextView.setTypeface(aquiline);
+
+        mNoteButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String note = mNoteEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, NoteActivity.class);
+                intent.putExtra("note", note);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/pinkjacket/swordslist/NoteActivity.java
+++ app/src/main/java/pinkjacket/swordslist/NoteActivity.java
@@ -1,13 +1,23 @@
 package pinkjacket.swordslist;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NoteActivity extends AppCompatActivity {
+    @BindView(R.id.noteTextView) TextView mNoteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+        ButterKnife.bind(this);
+        Intent intent = getIntent();
+        String note = intent.getStringExtra("note");
+        mNoteTextView.setText("Note: " + note);
     }
 }
