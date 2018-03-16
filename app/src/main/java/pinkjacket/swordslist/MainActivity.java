0f0f20d9caa19f9bb4cb60a764de9b4606f25683
Student <example@example.com>
2018-03-16 12:37:17 -0700

    both buttons now navigate to new activities

--- app/src/main/java/pinkjacket/swordslist/MainActivity.java
+++ app/src/main/java/pinkjacket/swordslist/MainActivity.java
@@ -37,5 +37,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mListButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
